88638f998cf151ec3f3ba56f4a4f900985428e3b
Zain-IIU <xainulabideen600@gmail.com>
2022-03-14 16:57:28 +0500

    Level 3 half done

---
+++ Spooky Friday/Assets/Game/Scripts/Arrow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("box"))
+        {
+            AudioManager.instance.Play("break");
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
